58f97ef9e67a59e7bd146a07c6179cbdd683ef68
ismet <ismntr@icloud.com>
2023-08-04 01:08:22 +0300

    first commit

---
+++ myproject/myapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
