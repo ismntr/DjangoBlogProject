8949e5366c5b43a327d981ec77a033e5fd44707f
ismet <ismntr@icloud.com>
2023-08-04 03:03:03 +0300

    projede bulunan Adım 1 tamamlandı ve çalışıyor

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        app_label = 'myapp'  
+
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(is_deleted=False)
+    #return render(request, 'post_list.html', {'posts': posts})
+    return render(request, 'myapp/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    # Burada post detayını almak için gerekli işlemleri yapın
+    return HttpResponse(f"Post Detail for Post ID: {pk}")
+
+
+
+
+    
