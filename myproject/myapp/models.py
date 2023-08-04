f7fa79baa17e75c9aa6baf4c3462d7650e0094d0
ismet <ismntr@icloud.com>
2023-08-04 04:13:04 +0300

    Projedeki 2. Adım da tamamlandı ve çalışıyor

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to='posts/', blank=True, null=True)
     is_deleted = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
 
     def __str__(self):
         return self.title
@@ -12,3 +17,5 @@ class Post(models.Model):
     class Meta:
         app_label = 'myapp'  
 
+
+  
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,14 +3,21 @@ from django.http import HttpResponse
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.filter(is_deleted=False)
-    #return render(request, 'post_list.html', {'posts': posts})
+    if request.user.is_authenticated:
+        # Eğer kullanıcı giriş yapmışsa, kendi postlarını listeleyelim
+        posts = Post.objects.filter(is_deleted=False, user=request.user)
+    else:
+        # Kullanıcı giriş yapmamışsa, tüm postları listeleyelim
+        posts = Post.objects.filter(is_deleted=False)
+    
     return render(request, 'myapp/post_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
     # Burada post detayını almak için gerekli işlemleri yapın
     return HttpResponse(f"Post Detail for Post ID: {pk}")
+
+
 
 
 
