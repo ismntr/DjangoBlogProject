ec772b9fee7f89c0b4ac6fad903908ba59560391
ismet <ismntr@icloud.com>
2023-08-09 21:24:28 +0300

    bug fix 2

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),  
     path('post/<int:pk>/', views.post_detail, name='post_detail'),  
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'), 
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'), 
     path('post/create/', views.post_create, name='post_create'), 
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 from .models import Post
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login, logout, authenticate
+#from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
 from django.contrib.auth.decorators import login_required
 from .forms import PostForm
 
@@ -27,34 +28,31 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            kullanici = form.save()
-            login(request, kullanici)
+            user = form.save()
+            login(request, user)
             return redirect('post_list')
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
 
-
-
-def login(request):
+def login_view(request):  
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            kullanici = authenticate(username=username, password=password)
-            if kullanici is not None:
-                login(request, kullanici)
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth_login(request, user)  
                 return redirect('post_list')
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
 
-
-
-def logout(request):
-    logout(request)
+def logout_view(request):  
+    auth_logout(request)  
     return redirect('login')
+
 
 
 
